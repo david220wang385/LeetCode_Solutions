ad858e32e3be3122ba99a6053f9c5cbb3dac81bc
David W <david220wang385@gmail.com>
2019-10-06 15:33:48 -0400

    Created background information for Happy Number Problem

---
+++ LeetCode_Solutions/IsHappyNumber.cpp
@@ -0,0 +1,15 @@
+/*
+A happy number is a number where the sum of the squares of the digits is eventually equal to 1
+Create an algorithm to determine whether a number 'n' is a happy number
+
+i.e.
+isHappyNumber(10) is true
+1^2 + 0^2 = 1
+
+isHappyNumber(7) is true
+7^2 = 49
+4^2 + 9^2 = 97
+9^2 + 7^2 = 130
+1^2 + 3^2 + 0^2 = 10
+1^2 + 0^2 = 1
+*/
